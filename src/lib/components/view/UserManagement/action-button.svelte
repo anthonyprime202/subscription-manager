90a2477a43ae9222af81eca45f299a4b03edf74f
anthonyprime202 <anthonyprime202@gmail.com>
2025-07-22 13:53:36 +0530

    Add delete user feature

--- src/lib/components/view/UserManagement/action-button.svelte
+++ src/lib/components/view/UserManagement/action-button.svelte
@@ -3,7 +3,11 @@
 	import { getContext } from "svelte";
 	import type { UserData } from "./columns";
 	import { Ellipsis, Pencil, Trash } from "@lucide/svelte";
+	import { useSheets } from "$lib/state/sheets.svelte";
+	import { postSheet } from "$lib/api";
+	import { toast } from "svelte-sonner";
 
+	const sheetState = useSheets();
 	let { currentRow }: { currentRow: UserData } = $props();
 	const dialogState: any = getContext(Symbol.for("dialog-state"));
 </script>
@@ -20,8 +24,26 @@
 					dialogState.typeForm = "Edit";
 				}}><Pencil size={15} color="var(--foreground)" />Edit</Dropdown.Item
 			>
-			<Dropdown.Item class="flex gap-2 text-destructive" onclick={() => {}}
-				><Trash size={15} color="var(--destructive)" />Delete</Dropdown.Item
+			<Dropdown.Item
+				class="flex gap-2 text-destructive"
+				onclick={async () => {
+					const row = sheetState.userSheet.find(
+						(s) => s.username === currentRow.username,
+					)!;
+
+					toast.promise(postSheet({
+						action: "delete",
+						rows: [{
+							...row
+						}
+						]
+					}), {
+						loading: "Deleting user",
+						success: () =>  { sheetState.updateUser(); return "User deleted successfully"},
+						error: "Failed to delete user",
+						
+					})
+				}}><Trash size={15} color="var(--destructive)" />Delete</Dropdown.Item
 			>
 		</Dropdown.Content>
 	</Dropdown.Root>
